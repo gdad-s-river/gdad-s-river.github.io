e63c61e5f7d33a78732cf4a9e07034de9b0eabdc
gdad-s-river <arihantverma1994@gmail.com>
2016-12-18 17:35:38 +0530

    indentation fixed + one typo fixed

--- sw.js
+++ sw.js
@@ -1,43 +1,44 @@
 const VERSION = "gdad-s-river-static-v1",
-	staticCacheName = "gdad-s-river";
+  staticCacheName = "gdad-s-river";
 
 console.log("installing service worker");
 
 const filesToCache = [
-	"/",
-	"/assets/images",
-	"css/main.css",
-	"/about/",
-	"/index.html"
-]
+  "/",
+  "/assets/images",
+  "css/main.css",
+  "/about/",
+  "/index.html"
+];
 
 self.addEventListener("install", function(e){
-	e.waitUntil(
-		caches.open(cacheName).then(function(cache) {
-			return cache.addAll(filesToCache);
-		})
-	)
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache){
+      return cache.addAll(filesToCache);
+    })
+  )
 });
 
 self.addEventListener("activate", function(e){
-	e.waitUntil(
-		caches.keys().then(function(cacheNames) {
-			return Promise.all(
-				cacheNames.filter(function(cacheName){
-					return cacheName.startsWith("gdad-s-river-")
-						&& cacheName != staticCacheName;
-				}).map(function(cacheName){
-					return cache.delete(cacheName);
-				})
-			)
-		})
-	)
+  e.waitUntil(
+    caches.keys().then(function(cacheNames){
+      return Promise.all(
+        cacheNames.filter(function(cacheName){
+          return cacheName.startsWith("gdad-s-river-")
+            && cacheName != staticCacheName;
+        }).map(function(cacheName){
+          return cache.delete(cacheName);
+        })
+      )
+    })
+  )
 });
 
 self.addEventListener("fetch", function(e){
-	e.respondWith(
-		caches.match(event.request).then(function(response){
-			return response || fetch(e.request);
-		})
-	)
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  )
 });
+
